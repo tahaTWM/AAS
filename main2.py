f0001f365dde99522883b6a64f72ce0d00d1b11e
tahaTWM <taha.twm.1234@gmail.com>
2021-12-24 11:35:35 +0300

    Fri 12/24/2021, 11:35:35.81

--- main2.py
+++ main2.py
@@ -1,55 +1,74 @@
-from pyzbar.pyzbar import decode
-import cv2
-import time
+from pyzbar.pyzbar import decode  # decode QrCode in each Frame
 
-from datetime import datetime
-import pandas as pd
-import xlsxwriter
+import cv2  # Use this package to use camera that connect to system
+
+import time  # buildIn class to make application sleep (delay)
+
+from datetime import datetime  # buildIn class to get current system time
+
+import pandas as pd  # use this package to deal with files (open and manipulate)
+
+import xlsxwriter  # use this package to write data exel sheet
 
 
-def AAS():
-    video = cv2.VideoCapture(0)
-    studentName = []
-    studentTime = []
-    studentAll = []
+def AAS():  # main function of the application
 
-    now = datetime.now()
-    today = now.strftime("%d_%m_%Y-%H_%M")
+    video = cv2.VideoCapture(0)  # 0 for selecting the build web camera
+    
+    studentName = []  # list of student signIn time
+    studentTime = []  # list of student that signIn
+    studentAll = []  # mix students names withe time to store it.
+
+    now = datetime.now()  # get current system time
+    today = now.strftime("%d_%m_%Y-%H_%M")  # format system time to (24_12_2021-11_14)
 
     print("Today's date: {}".format(today))
 
-    def readData():
-        data = pd.read_excel("stuent.xlsx", "Sheet1")
+    def readData():  # read students data form exel sheet
+        data = pd.read_excel("students.xlsx", "Sheet1")
         names = data["Names"].values.tolist()
         dept = data["Department"].values.tolist()
         return names, dept
 
-    def writeData():
-        workbook = xlsxwriter.Workbook("{}.xlsx".format(today))
-        worksheet = workbook.add_worksheet()
+    def writeData():  # write or storing data to exel sheet
+        if len(studentName) > 0:
+            workbook = xlsxwriter.Workbook("{}.xlsx".format(today))
+            worksheet = workbook.add_worksheet()
 
-        row = 1
-        worksheet.write_column(0, 0, ["Names"])
-        worksheet.write_column(0, 1, ["Times"])
-        for col, data in enumerate(studentAll):
-            worksheet.write_column(row, col, data)
+            # adding header or first row of exel sheet like ---  Names   Times --- in next 2 line
+            worksheet.write_column(0, 0, ["Names"])
+            worksheet.write_column(0, 1, ["Times"])
 
-        workbook.close()
+            studentAll.append(studentName)
+            studentAll.append(studentTime)
+            print(studentAll)
+
+            row = 1  # starting in second row in exel sheet
+            for col, data in enumerate(studentAll):
+                worksheet.write_column(row, col, data)
+
+            workbook.close()
 
     names, dept = readData()
     while True:
         check, frame = video.read()
         dcode = decode(frame)
-
         try:
-            for obj in dcode:
-                name = dcode[0].data.decode()
+            for items in dcode:
+                name = items[0].decode()
+                # jsonLoad = json.loads(name)
+                # print(jsonLoad['name'])
+
                 if name not in names:
-                    print("Not found, this Student is from this Department")
+                    print(
+                        f"{name}{', Is Not found in this Students of this Department'}"
+                    )
+                    time.sleep(1)
                 if name in names:
                     studentName.append(name)
                     studentTime.append(today)
                     print("add is done")
+                    print(studentName)
                     time.sleep(1)
 
         except:
@@ -59,14 +78,12 @@ def AAS():
         key = cv2.waitKey(1)
         if key == ord("q"):
             print("Stopped")
-            studentAll.append(studentName)
-            studentAll.append(studentTime)
-            if len(studentAll) > 2:
-                writeData()
+            writeData()
             break
 
     video.release()
     cv2.destroyAllWindows()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     AAS()
